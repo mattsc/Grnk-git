2b60b4658a6d1b0242b3dcb5616a1d90e93bb7eb
mattsc <mattsc000@gmail.com>
2014-08-21 10:42:18 -0700

    Lizards AI: remove dependence on AI-demos

--- lua/ca_lizards.lua
+++ lua/ca_lizards.lua
@@ -1,6 +1,6 @@
 local H = wesnoth.require "lua/helper.lua"
 local W = H.set_wml_action_metatable {}
-local AH = wesnoth.require "~/add-ons/AI-demos/lua/ai_helper.lua"
+local AH = wesnoth.require "ai/lua/ai_helper.lua"
 local LS = wesnoth.require "lua/location_set.lua"
 
 local hold_area = {}
