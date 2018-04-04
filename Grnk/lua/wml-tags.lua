204f7aab53391d44234f01c084591e59c5ed70ec
mattsc <mattsc000@gmail.com>
2018-04-04 13:06:52 -0700

    [dialog_message]: use new Lua function for Wesnoth 1.13
    This also eliminates the need for helper.set_wml_tag_metatable, which
    is deprecated in 1.13.

--- Grnk/lua/wml-tags.lua
+++ Grnk/lua/wml-tags.lua
@@ -1,28 +1,32 @@
-local helper = wesnoth.require "lua/helper.lua"
-local T = helper.set_wml_tag_metatable {}
-local _ = wesnoth.textdomain "wesnoth-Grnk"
 
 function wesnoth.wml_actions.dialog_message(cfg)
     -- Set up tag [dialog_message]
     -- Displays centered, framed, clickable message
     -- Only key: message
 
-    local message=(cfg.message)
-
-    local dialog = {
-        T.tooltip { id = "tooltip_large" },
-        T.helptip { id = "tooltip_large" },
-        T.grid {
-            T.row { T.column { horizontal_alignment = "left",
-                border = "all",
-                border_size = 5,
-                T.label { label = message } } }
-        },
-        click_dismiss = true
-    }
-
+    local message = tostring(cfg.message)
     wesnoth.delay(20)
-    wesnoth.show_dialog(dialog)
+
+    if wesnoth.compare_versions(wesnoth.game_config.version, '>=', '1.13.2') then
+        wesnoth.show_popup_dialog(message)
+    else
+        local helper = wesnoth.require "lua/helper.lua"
+        local T = helper.set_wml_tag_metatable {}
+
+        local dialog = {
+            T.tooltip { id = "tooltip_large" },
+            T.helptip { id = "tooltip_large" },
+            T.grid {
+                T.row { T.column { horizontal_alignment = "left",
+                    border = "all",
+                    border_size = 5,
+                    T.label { label = message } } }
+            },
+            click_dismiss = true
+        }
+
+        wesnoth.show_dialog(dialog)
+    end
 end
 
 function wesnoth.wml_actions.dialog_defeat(cfg)
